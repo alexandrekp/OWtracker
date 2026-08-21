92e2a9f52f7d2e18d47c47fe027aa3a03f94c111
AKP <171920496+alexandrekp@users.noreply.github.com>
2026-08-21 22:12:28 +0200

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,27 @@ import react from "@vitejs/plugin-react";
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
+// GitHub Pages project path
+const base =
+  process.env.GITHUB_ACTIONS ||
+  process.env.NODE_ENV === "production"
+    ? "/OWtracker/"
+    : "/";
+
 // https://vite.dev/config/
 export default defineConfig(async () => ({
   plugins: [react()],
 
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent Vite from obscuring rust errors
+  base,
+
+  // Vite options tailored for Tauri development
   clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
+
   server: {
     port: 1420,
     strictPort: true,
     host: host || false,
+
     hmr: host
       ? {
           protocol: "ws",
@@ -24,9 +32,11 @@ export default defineConfig(async () => ({
           port: 1421,
         }
       : undefined,
+
     watch: {
-      // 3. tell Vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
+      ignored: [
+        "**/src-tauri/**",
+      ],
     },
   },
 }));
